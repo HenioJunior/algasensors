b1026886000c77acc9db65ad99dc0f2e6d8c7f1f
Henio Junior <henioajunior@gmail.com>
2026-04-16 05:43:39 -0300

    feat(sensor): add looped temperature sending and Kafka producer configurations
    - Enhanced `TemperatureController` to support looped temperature sending with start/stop endpoints.
    - Updated Kafka producer factory to use `JsonSerializer` and explicitly defined Kafka configurations.

--- microservices/sensor/src/main/kotlin/com/algasensors/sensor/controller/TemperatureController.kt
+++ microservices/sensor/src/main/kotlin/com/algasensors/sensor/controller/TemperatureController.kt
@@ -4,18 +4,40 @@ import com.algasensors.sensor.kafka.TemperatureProducer
 import com.algasensors.sensor.service.TemperatureService
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.concurrent.thread
 
 @RestController
+@RequestMapping("/api/sensor/")
 class TemperatureController(
     private val temperatureProducer: TemperatureProducer,
     private val temperatureService: TemperatureService
 ) {
+    // Flag para controlar o loop de envio
+    private var isSending = false
 
-    @PostMapping("/sendTemperature/{sensorId}")
+    @PostMapping("sendTemperature/{sensorId}")
     fun sendTemperature(@PathVariable sensorId: String): String {
-        val temperature = temperatureService.generateTemperature()
-        temperatureProducer.sendTemperature(sensorId, temperature)
-        return "Temperature sent: $temperature"
+        if (!isSending) {
+            // Iniciar o envio em loop
+            isSending = true
+            thread(start = true) {
+                while (isSending) {
+                    val temperature = temperatureService.generateTemperature()
+                    temperatureProducer.sendTemperature(sensorId, temperature)
+                    println("Temperature sent: $temperature")
+                    Thread.sleep(3000) // Espera 30 segundos
+                }
+            }
+            return "Temperature sending started"
+        }
+        return "Temperature sending is already running"
+    }
+
+    @PostMapping("stopSending")
+    fun stopSending(): String {
+        isSending = false
+        return "Temperature sending stopped"
     }
 }
--- microservices/sensor/src/main/kotlin/com/algasensors/sensor/kafka/KafkaProducerConfig.kt
+++ microservices/sensor/src/main/kotlin/com/algasensors/sensor/kafka/KafkaProducerConfig.kt
@@ -1,6 +1,8 @@
 package com.algasensors.sensor.kafka
 
 import org.apache.kafka.clients.admin.NewTopic
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,6 +11,7 @@ import org.springframework.kafka.config.TopicBuilder
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.serializer.JsonSerializer
 
 @Configuration
 @EnableKafka
@@ -27,8 +30,12 @@ class KafkaProducerConfig {
 
     @Bean
     fun producerFactory(): ProducerFactory<String, TemperatureMessage> {
-        // Ao não passar um Map, o Spring usa as configurações do application.yaml
-        return DefaultKafkaProducerFactory(emptyMap())
+        val configs = mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java  // Usando JsonSerializer para o valor
+        )
+        return DefaultKafkaProducerFactory(configs)
     }
 
     @Bean
