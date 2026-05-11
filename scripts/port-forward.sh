f0c966a3b330e5d0f9975a27ec494449c1ac0048
Henio Junior <henioajunior@gmail.com>
2026-05-11 08:41:24 -0300

    feat(k8s): add Kubernetes configurations for `temperature-monitoring`
    - Introduced Deployment and Service resources for `temperature-monitoring` in the `algasensors` namespace.
    - Added `application-k8s.yml` with PostgreSQL and Kafka configurations.
    - Created Dockerfile for the `temperature-monitoring` service.
    - Updated Kafka listener configurations to support external access.
    - Extended `port-forward.sh` script to include `temperature-monitoring` and other services.

--- scripts/port-forward.sh
+++ scripts/port-forward.sh
@@ -5,7 +5,7 @@ echo "Starting port-forwards..."
 kubectl port-forward svc/postgres 5432:5432 -n algasensors > /dev/null 2>&1 &
 echo "Postgres -> localhost:5432"
 
-kubectl port-forward svc/broker 9092:29092 -n algasensors > /dev/null 2>&1 &
+kubectl port-forward svc/broker 9092:9092 -n algasensors > /dev/null 2>&1 &
 echo "Kafka -> localhost:9092"
 
 kubectl port-forward svc/pgadmin 8090:80 -n algasensors > /dev/null 2>&1 &
@@ -13,6 +13,15 @@ echo "PgAdmin -> localhost:8090"
 
 kubectl port-forward svc/kafka-ui 8080:8080 -n algasensors > /dev/null 2>&1 &
 echo "Kafka UI -> localhost:8080"
+
+kubectl port-forward svc/temperature-processing 8084:8084 -n algasensors > /dev/null 2>&1 &
+echo "Temperature Processing -> localhost:8084"
+
+kubectl port-forward svc/temperature-monitoring 8083:8083 -n algasensors > /dev/null 2>&1 &
+echo "Temperature Monitoring -> localhost:8083"
+
+kubectl port-forward svc/sensor-simulator 8082:8082 -n algasensors > /dev/null 2>&1 &
+echo "SensorSimulator -> localhost:8082"
 
 echo ""
 echo "Port-forwards started."
