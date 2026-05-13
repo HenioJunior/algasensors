8dff2703f1c179735a07ca335ec96dd80b366b71
Henio Junior <henioajunior@gmail.com>
2026-05-13 09:21:06 -0300

    feat(kafka): configure Kafka topics for temperature data processing and update port-forward script
    - Added `KafkaTopicConfig` with topic definitions for raw and processed temperature readings.
    - Commented out unused port-forwarding commands in `05_port-forward.sh` to declutter script.

--- k8s/scripts/05_port-forward.sh
+++ k8s/scripts/05_port-forward.sh
@@ -14,14 +14,14 @@ echo "PgAdmin -> localhost:8090"
 kubectl port-forward svc/kafka-ui 8080:8080 -n algasensors > /dev/null 2>&1 &
 echo "Kafka UI -> localhost:8080"
 
-kubectl port-forward svc/temperature-processing 8084:8084 -n algasensors > /dev/null 2>&1 &
-echo "Temperature Processing -> localhost:8084"
-
-kubectl port-forward svc/temperature-monitoring 8083:8083 -n algasensors > /dev/null 2>&1 &
-echo "Temperature Monitoring -> localhost:8083"
-
-kubectl port-forward svc/sensor-simulator 8082:8082 -n algasensors > /dev/null 2>&1 &
-echo "SensorSimulator -> localhost:8082"
+#kubectl port-forward svc/temperature-processing 8084:8084 -n algasensors > /dev/null 2>&1 &
+#echo "Temperature Processing -> localhost:8084"
+#
+#kubectl port-forward svc/temperature-monitoring 8083:8083 -n algasensors > /dev/null 2>&1 &
+#echo "Temperature Monitoring -> localhost:8083"
+#
+#kubectl port-forward svc/sensor-simulator 8082:8082 -n algasensors > /dev/null 2>&1 &
+#echo "SensorSimulator -> localhost:8082"
 
 echo ""
 echo "Port-forwards started."
